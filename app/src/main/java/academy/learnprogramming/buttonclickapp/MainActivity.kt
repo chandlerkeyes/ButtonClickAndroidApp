7a1940c052d6d962a5e4cd9a15cae5f39bb73167
Chandler Keyes <chandlerkeyes@Chandlers-MacBook-Pro.local>
2018-12-16 22:16:48 -0500

    text view displays userInput onClick

--- app/src/main/java/academy/learnprogramming/buttonclickapp/MainActivity.kt
+++ app/src/main/java/academy/learnprogramming/buttonclickapp/MainActivity.kt
@@ -3,37 +3,39 @@ package academy.learnprogramming.buttonclickapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Button
 
+private val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     /*initializing these variables as null starting out, the below widgets don't exist yet since we haven't set the
      content view. First, we set them to null by default, then once the program recognizes the widgets, we'll reassign them to
      the proper variable. */
-    private var userInput : EditText? = null
-    private var button: Button? = null
-    private var textView : TextView? = null
-    private var numTimesClicks = 0
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
-        textView?.text = ""
+        textView?.text = null
         textView?.movementMethod = ScrollingMovementMethod()
+        userInput.setText("")
 
-        button?.setOnClickListener(object : View.OnClickListener {
+        button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                numTimesClicks += 1
-                textView?.append("\nThe button got tapped $numTimesClicks time")
-                if (numTimesClicks != 1) {
-                    textView?.append("s")
-                }
+                Log.d(TAG, "onClick: called")
+                textView?.append(userInput.text)
+                textView?.append("\n")
+                //userInput.text.clear()
+                userInput.setText("")
             }
         })
     }
